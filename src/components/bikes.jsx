cd57c7ff2aef811503bc0ff4568263645a489542
Mary Ella Hill <hillmaryella@gmail.com>
2019-11-07 15:37:42 -0500

    Both things on page

--- src/components/bikes.jsx
+++ src/components/bikes.jsx
@@ -7,7 +7,8 @@ import { loadData } from "../util/loadData";
 export class BikeInfo extends Component {
     state = {
         titles: [],
-        descriptions: []
+        descriptions: [],
+        incidents: []
     }
 
     async componentDidMount(){
@@ -18,36 +19,52 @@ export class BikeInfo extends Component {
         const response = await loadData(`https://bikewise.org:443/api/v2/locations/markers?proximity=atlanta&proximity_square=100`)
         let titleList = []
         let descriptionList = []
+        let incidentList = []
         for (let i = 0; i < response.features.length; i++){
             let title = response.features[i]['properties']['title']
             let description = response.features[i]['properties']['description']
+            let incident = response.features[i]['properties']
+            
             titleList.push(title)
-            descriptionList.push(description);
+            descriptionList.push(description)
+            incidentList.push(incident)
         }
         this.setState({
-            titles: titleList,
-            descriptions: descriptionList
+            incidents: incidentList,
+            descriptions: descriptionList,
+            titles: titleList
         })
     };
 
     render () {
         const display_titles = this.state.titles;
         const display_descriptions = this.state.descriptions;
-        console.log(display_descriptions)
+       
+        console.log("console log")
         return (
-            <>
-          
-            {display_titles.map((incident, id) => {
+            <div>
+                
+                 {display_titles.map((incident, id) => {
                     return ( 
-                        <h3 key={`incident-${id}`}>
+                        <h2 key={`incident-${id}`}>
                             {incident}
-                        </h3>
+                        </h2>
               
                     );
                 })}
+                
+                 {display_descriptions.map((incident, id) => {
+                    return ( 
+                        <p key={`incident-${id}`}>
+                            {incident}
+                        </p>
+              
+                    );
+                })}
+               
+          
+             </div>
            
-     
-            </>
         );
     }
 }
