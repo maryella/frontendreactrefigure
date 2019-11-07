d53f7cda9dbdf699366cb01c24cab17e453079af
Mary Ella Hill <hillmaryella@gmail.com>
2019-11-07 11:12:37 -0500

    Able to get one result  from API on screen

--- src/components/bikes.jsx
+++ src/components/bikes.jsx
@@ -4,22 +4,35 @@ import { loadData } from "../util/loadData";
 
 
 export class BikeInfo extends Component {
+    state = {
+        title: "Loading title",
+        description: "Loading description",
+    }
 
     async componentDidMount(){
         this.getInfo()
     };
 
     getInfo = async () => {
-        const data = await loadData(`https://bikewise.org:443/api/v2/locations/markers?proximity=atlanta&proximity_square=100`)
-        const titleInfo = (data.features[0]['properties']['title'])
-        console.log(titleInfo)
-        return data
+        const response = await loadData(`https://bikewise.org:443/api/v2/locations/markers?proximity=atlanta&proximity_square=100`)
+        const inctitle = response.features[0]['properties']['title']
+        const incdescription = response.features[0]['properties']['description']
+
+        this.setState({
+            title : inctitle,
+            description: incdescription
+        })
     };
 
     render () {
-        
+        const display_title = this.state.title;
+        const display_description = this.state.description;
+        console.log (this.state.title)
         return (
-            <div></div>
+            <div>
+            <h3>{display_title}</h3>
+            <p>{display_description}</p>
+            </div>
         );
     }
 }
